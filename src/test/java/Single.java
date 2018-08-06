a7049e0e0ed0533bbd54ead6cb74de2eec487733
wenliang.li <wenliang.li@qunar.com>
2018-08-06 10:35:11 +0800

    Lettuce debug

--- src/test/java/Single.java
+++ src/test/java/Single.java
@@ -4,7 +4,6 @@ import com.lambdaworks.redis.RedisFuture;
 import com.lambdaworks.redis.RedisURI;
 import com.lambdaworks.redis.api.StatefulRedisConnection;
 import com.lambdaworks.redis.api.async.RedisAsyncCommands;
-import com.lambdaworks.redis.api.sync.RedisCommands;
 
 public class Single
 {
@@ -15,35 +14,45 @@ public class Single
 
         // 创建集Redis单机模式客户端
         RedisClient redisClient = RedisClient.create(redisURI);
-            ClientOptions clientOptions = ClientOptions.builder().autoReconnect(true).disconnectedBehavior(ClientOptions.DisconnectedBehavior.DEFAULT).requestQueueSize(100).cancelCommandsOnReconnectFailure(true).build();
+            ClientOptions clientOptions = ClientOptions.builder().autoReconnect(true).disconnectedBehavior(ClientOptions.DisconnectedBehavior.DEFAULT).requestQueueSize(10).cancelCommandsOnReconnectFailure(true).build();
         redisClient.setOptions(clientOptions);
 
-        // 创建集Redis单机模式客户端
-        RedisClient redisClient1 = RedisClient.create(redisURI);
-        ClientOptions clientOptions1 = ClientOptions.builder().autoReconnect(true).disconnectedBehavior(ClientOptions.DisconnectedBehavior.DEFAULT).requestQueueSize(100).cancelCommandsOnReconnectFailure(true).build();
-        redisClient1.setOptions(clientOptions1);
 
         // 开启连接
         StatefulRedisConnection<String, String> connect = redisClient.connect();
         RedisAsyncCommands<String, String> cmd = connect.async();
 
-        StatefulRedisConnection<String, String> connect1 = redisClient1.connect();
-        RedisCommands<String, String> cmd1 = connect1.sync();
 
-//        cmd.hset("maizi","skr","666");
+        for(int a = 0; a < 20; a++) {
+            for (int i = 0; i <= 10; i++) {
 
-        for(int i = 0; i < 10000; i++){
-            Thread.sleep(10);
-            try{
+                try {
 
-                RedisFuture<Boolean> redisFuture = cmd.hset("maizi","haha",i+"");
-                RedisFuture<Boolean> redisFuture2 = cmd.hset("maizi","skr",i+"");
-                System.out.println(redisFuture);
-            }catch (Exception e){
-                e.printStackTrace();
+                    RedisFuture<Boolean> redisFuture = cmd.hset("maizi", "haha", i + "");
+                    System.out.println(redisFuture);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
+
+            Thread.sleep(10);
         }
 
+        Thread.sleep(10000);
+        for(int a = 0; a < 1; a++) {
+            for (int i = 0; i <= 10; i++) {
+
+                try {
+
+                    RedisFuture<Boolean> redisFuture = cmd.hset("maizi", "haha", i + "");
+                    System.out.println(redisFuture);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            Thread.sleep(1000);
+        }
 
 
 
