4b3af295a0c7d36dd4fa2029020828477c9d8e4c
wenliang.li <wenliang.li@qunar.com>
2018-08-06 16:53:06 +0800

    Lettuce debug

--- src/test/java/Single.java
+++ src/test/java/Single.java
@@ -19,47 +19,31 @@ public class Single
             ClientOptions clientOptions = ClientOptions.builder().autoReconnect(true).disconnectedBehavior(ClientOptions.DisconnectedBehavior.DEFAULT).requestQueueSize(10).cancelCommandsOnReconnectFailure(true).build();
         redisClient.setOptions(clientOptions);
 
-
         // 开启连接
         StatefulRedisConnection<String, String> connect = redisClient.connect();
-        RedisAsyncCommands<String, String> cmd = connect.async();
 
 
-        for(int a = 0; a < 20; a++) {
-            for (int i = 0; i <= 10; i++) {
-
+        //每次传入大于队列大小的命令，使生产速度大于消费速度，以造成Internal stack size exceeded 的异常，产生空命令，多循环几次，使空命令占满请求队列
+        for(int j = 0; j < 200; j++) {
+            for (int i = 0; i < 15; i++) {
                 try {
-
-                    RedisFuture<Boolean> redisFuture = cmd.hset("maizi", "haha", i + "");
-//                    System.out.println(redisFuture);
+                    RedisFuture<Boolean> redisFuture = connect.async().hset("maizi", "haha", i + "");
                 } catch (Exception e) {
-//                    e.printStackTrace();
-
                 }
             }
-
             Thread.sleep(10);
-
         }
         System.out.println("===================");
-
         Thread.sleep(10000);
-        for(int a = 0; a < 1; a++) {
-            for (int i = 0; i <= 10; i++) {
-
-                try {
-
-                    RedisFuture<Boolean> redisFuture = cmd.hset("maizi", "haha", i + "");
-                    System.out.println(redisFuture);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        for(int x = 0; x < 100000; x++) {
+            try {
+                RedisFuture<Boolean> redisFuture = connect.async().hset("maizi", "haha", x + "");
+                System.out.println(redisFuture);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
             Thread.sleep(1000);
         }
-
-
 
     }
 }
